c8f3a5b242fa9f4f0656284ee39942c704fab584
FidyBack <abel.cavalcante.ismart@gmail.com>
2019-10-17 07:39:57 -0300

    "Updates"

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect(
+    'mongodb://localhost:27017/lolapi',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.get('/', (req, res) => {
     res.send('Hi');

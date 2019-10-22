14c02c39681e7f2872190986b6540f3d5e40fb4b
FidyBack <abel.cavalcante.ismart@gmail.com>
2019-10-21 21:38:25 -0300

    Algo

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
+// DataBase
 mongoose.connect(
-    'mongodb://localhost:27017/lolapi',
+    'mongodb://localhost:27017/lol-api',
     { useNewUrlParser: true, useUnifiedTopology: true }
 );
+requireDir('./src/models');
 
-app.get('/', (req, res) => {
-    res.send('Hi');
-});
+// Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000!');
